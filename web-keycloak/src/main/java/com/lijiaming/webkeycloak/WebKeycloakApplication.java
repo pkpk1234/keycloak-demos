9db7603e68569cdbff2b9877010bd6107d1d246f
lijiaming <lijiaming1984@gmail.com>
2018-02-23 22:15:28 +0800

    初始化

---
+++ web-keycloak/src/main/java/com/lijiaming/webkeycloak/WebKeycloakApplication.java
@@ -0,0 +1,12 @@
+package com.lijiaming.webkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebKeycloakApplication.class, args);
+	}
+}
